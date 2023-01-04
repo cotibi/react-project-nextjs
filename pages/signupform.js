5e69236afe35d89cb7c77d5535993d7387442c60
Tibi <co.tibi@gmail.com>
2023-01-04 21:34:13 +0200

    Added return home logo

--- pages/signupform.js
+++ pages/signupform.js
@@ -1,10 +1,10 @@
 import styles from '../styles/signinform.module.css';
 import Link from 'next/link';
 
-const Signinform = () => (
+const Signupform = () => (
 
     <div className={styles.maincontainer}>
-        <h1 className={styles.signinheader}>Sign in</h1>
+        <h1 className={styles.signinheader}>Sign up</h1>
         <div className={styles.inputcontainer}>
 
             <label className={styles.label} for="email"><b>Email</b></label>
@@ -13,17 +13,19 @@ const Signinform = () => (
             <label className={styles.label} for="psw"><b>Password</b></label>
             <input className={styles.input} type="password" placeholder="Enter password" name="psw" required></input>
 
+            <label className={styles.label} for="psw"><b>Confirm password</b></label>
+            <input className={styles.input} type="password" placeholder="Confirm password" name="psw" required></input>
+
             <input className={styles.submit} type="submit" name="submit "></input>
 
         </div>
 
-        <h3>Don't have an account yet?
-            <Link href="/signupform"> Sign up!</Link>
+        <h3>Already a registerd user?
+            <Link href="/signinform"> Sign in!</Link>
         </h3>
         <div>
-            <Link href="/"><img className={styles.logohome} alt="regrow-logo" src="https://svgshare.com/i/hyb.svg" title="regrow-logo" /></Link>
+        <Link href="/"><img className={styles.logohome} alt="regrow-logo" src="https://svgshare.com/i/hyb.svg" title="regrow-logo" /></Link>
         </div>
-
     </div>
 );
-export default Signinform;
+export default Signupform;
